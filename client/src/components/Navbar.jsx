831ad720e39c6bd89ace9f4bde610bbb6016e666
Martí Puig <104025496+martipuigmartin@users.noreply.github.com>
2022-06-01 15:07:58 +0200

    Update Navbar.jsx

--- client/src/components/Navbar.jsx
+++ client/src/components/Navbar.jsx
@@ -28,7 +28,7 @@ const Navbar = () => {
                     </li>
                     <li
                         className="bg-[#2952e3] py-2 px-7 mx-4 rounded-full cursor-pointer hover:bg-[#2546bd]">
-                        <a href="https://github.com/MartiPuigGinebro/ethereum-exchange">Source</a>
+                        <a href="https://github.com/martipuigmartin/ethereum-exchange">Source</a>
                     </li>
                 </ul>
 
